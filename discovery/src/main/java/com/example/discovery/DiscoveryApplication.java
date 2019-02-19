1cae951d32dd096d5a4bd5af901b31bf19d3a41f
gaoxiyang <gaoxiyang1237@126.com>
2019-02-19 10:15:43 +0800

    添加注释

--- discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/example/discovery/DiscoveryApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Spring Cloud 5分钟搭建教程(附上一个分布式日志系统项目作为参考)
+ * https://blog.csdn.net/lc0817/article/details/53266212/
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryApplication {
